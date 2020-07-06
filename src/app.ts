42c45c4b82adce8d2b179d82cd1cbda55fda074e
Bryan Opfer <824368+bopfer@users.noreply.github.com>
2020-07-06 12:33:44 -0700

    Simplify app

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,12 @@ import express from 'express';
 import { schema } from './graphql/schema';
 
 const app = express();
-app.disable(`x-powered-by`);
-
-const domain = `redacted.com`
-
-export const corsOptions = {
-  origin: new RegExp(`.${domain}$`),
-  credentials: true,
-  optionsSuccessStatus: 204,
-  maxAge: 600,
-};
 
 const apolloServer = new ApolloServer({
   schema,
   cacheControl: true,
 });
 
-apolloServer.applyMiddleware({ app, path: `/`, cors: corsOptions });
+apolloServer.applyMiddleware({ app, path: `/` });
 
 export { app };
